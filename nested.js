2eb0f86757a6622a0719f711ebc84467536a2889
TonmoyShahriar23 <tonmoy15-6052@diu.edu.bd>
2025-06-26 12:27:12 +0600

    Nested

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 10;
+if(money>300){
+    console.log('Rich');
+}
+else{
+    if(money>100){
+        console.log('Gorib Na');
+    }
+    else{
+        if(money>0){
+            console.log('Pani Kha');
+        }
+        else{
+            console.log('Tui Gorib');
+        }
+    }
+}
