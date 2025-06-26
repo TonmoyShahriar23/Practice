2e68b4861c26d8c7045b3c0a22f94c7b04d96ae9
TonmoyShahriar23 <tonmoy15-6052@diu.edu.bd>
2025-06-26 12:45:38 +0600

    Nested

--- nested.js
+++ nested.js
@@ -1,17 +1,46 @@
-const money = 10;
-if(money>300){
-    console.log('Rich');
-}
-else{
-    if(money>100){
-        console.log('Gorib Na');
+// const money = 10;
+// if(money>300){
+//     console.log('Rich');
+// }
+// else{
+//     if(money>100){
+//         console.log('Gorib Na');
+//     }
+//     else{
+//         if(money>0){
+//             console.log('Pani Kha');
+//         }
+//         else{
+//             console.log('Tui Gorib');
+//         }
+//     }
+// }
+
+
+var mark=81;
+var friendMark=35;
+if(mark>80){
+    if(friendMark>80){
+        console.log('go for lunch')
     }
     else{
-        if(money>0){
-            console.log('Pani Kha');
+        if(friendMark<80 && friendMark>=60){
+            console.log('Good luck next time ')
         }
         else{
-            console.log('Tui Gorib');
+            if(friendMark<60 && friendMark>=40){
+                console.log('Keep message unseen')
+            }
+            else(friendMark<40)
+                {
+                    console.log('Block Friend')
+                }
+            
+                
+            }
         }
     }
+
+else{
+    console.log('sleep and act sad')
 }
